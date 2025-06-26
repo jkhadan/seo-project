e56613f2c2e2ac96327712e87f63fcb0872517e8
James Khadan <jameskhadan@gmail.com>
2025-06-26 02:20:56 +0000

    added sql support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import sys
+import sqlalchemy as db
+import pandas as pd
 
 api_key = sys.argv[1]
 symbol = 'AAPL'
@@ -8,6 +10,14 @@ response = requests.get(url=f'https://api.marketstack.com/v2/eod?access_key={api
 
 data = response.json()['data']
 
-print('Apple (AAPL): Stock Closing Report')
-for a in data:
-  print(f'Date: {a["date"]} Close Price: {a["close"]}')
+# print('Apple (AAPL): Stock Closing Report')
+# for a in data:
+#   print(f'Date: {a["date"]} Close Price: {a["close"]}')
+
+# Day 2
+dataframe = pd.DataFrame(data)
+engine = db.create_engine('sqlite:///data_base_name.db')
+dataframe.to_sql('table_name', con=engine, if_exists='replace', index=False)
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
